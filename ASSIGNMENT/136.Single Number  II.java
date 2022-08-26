096416fe872b38a34ce25eac99ba69c871f48fc6
sneha <88367056+sneha512@users.noreply.github.com>
2022-08-26 23:49:01 +0530

    Create 136.Single Number  II.java

---
+++ ASSIGNMENT/136.Single Number  II.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int x=0;
+        int y=0;
+        for(int n:nums){
+            x=x^n & ~y;
+            y=y^n & ~x;
+        }
+        return x;
+    }
+}  
