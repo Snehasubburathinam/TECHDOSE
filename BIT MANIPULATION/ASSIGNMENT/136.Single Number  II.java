61280c4e994955557fd54a03b646226904b75fd1
sneha <88367056+sneha512@users.noreply.github.com>
2022-08-28 11:07:17 +0530

    Create 136.Single Number  II.java

---
+++ BIT MANIPULATION/ASSIGNMENT/136.Single Number  II.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int x=0;
+        int y=0;
+        for(int n:nums){
+            x=x^n & ~y;
+            y=y^n & ~x;
+        }
+        return x;
+    }
+}  
+Footer
+© 2022 GitHub, Inc.
