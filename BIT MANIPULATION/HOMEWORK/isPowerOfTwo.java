3dabba01787573e94eeb700d99b58ead2e385b1b
sneha <88367056+sneha512@users.noreply.github.com>
2022-09-06 18:35:16 +0530

    Create isPowerOfTwo.java

---
+++ BIT MANIPULATION/HOMEWORK/isPowerOfTwo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+
+  public static boolean isPowerOfTwo(int n){
+       return ((n!=0) && ((n&(n-1))==0);
+  }
+  
+  public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     System.out.println(isPowerOfTwo(n));
+  }
+}
