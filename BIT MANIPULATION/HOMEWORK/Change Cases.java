a48972ebdecba5c4d30427c29ffac1edd8c2c968
sneha <88367056+sneha512@users.noreply.github.com>
2022-09-06 18:26:37 +0530

    Update Change Cases.java

--- BIT MANIPULATION/HOMEWORK/Change Cases.java
+++ BIT MANIPULATION/HOMEWORK/Change Cases.java
@@ -1,1 +1,19 @@
-
+import java.util.*;
+class Main { 
+  public static char toLowerCase(char ch){
+    return  (char)((ch) | (1<<5));
+  }
+  public static char toUpperCase(char ch){
+    return  (char)((ch) & (~(1<<5)));
+  }
+  public static char toToggleCase(char ch){
+    return (char)(ch ^ (1<<5));
+  }
+  public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     char ch=sc.next().charAt(0);
+     System.out.println(toLowerCase(ch));  
+     System.out.println(toUpperCase(ch));
+     System.out.println(toToggleCase(ch));
+  }
+}
