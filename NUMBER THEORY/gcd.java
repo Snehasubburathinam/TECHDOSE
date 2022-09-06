aa117d3f00f0d6f94a87e4e9b41a04904df2a8cc
sneha <88367056+sneha512@users.noreply.github.com>
2022-09-06 21:14:50 +0530

    Create gcd.java

---
+++ NUMBER THEORY/gcd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+  static int GCD(int a,int b){
+    return (a==0)?b:GCD(b%a,a);
+  }
+  public static void main(String []args){
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    System.out.println(GCD(a,b));
+  }
+}
