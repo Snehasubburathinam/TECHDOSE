e788883a2633ce6650d810da08032c3a15412e26
sneha <88367056+sneha512@users.noreply.github.com>
2022-09-14 20:17:38 +0530

    Update 22.String to Integer(leedcode 8).java

--- BASICS/22.String to Integer(leedcode 8).java
+++ BASICS/22.String to Integer(leedcode 8).java
@@ -1,24 +1,28 @@
-import java.util.*;
-class Main {
-  public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    String s=sc.nextLine();
-    int []freq=new int[s.length()];
-    int i,j;
-    char str[]=s.toCharArray();
-    for(i=0;i<s.length();i++){
-    freq[i]=1;
-      for(j=i+1;j<s.length();j++){
-        if(str[i]==str[j]){
-          freq[i]++;
-          str[j]='0';
+class Solution {
+    public int myAtoi(String str) {
+        int i = 0;
+        int res = 0;
+        int sign = 1;
+        
+        if(str.length() == 0) return 0;
+      
+        while(i < str.length() && str.charAt(i) == ' ')
+            i++;
+        if (i == str.length()) return 0;
+        if(str.charAt(i) == '+' || str.charAt(i) == '-') {
+            sign = str.charAt(i) == '+' ? 1 : -1;
+            i++;
         }
-      }
+        while(i < str.length()) {
+            int digit = str.charAt(i) - '0';
+            if(digit < 0 || digit > 9) break;
+            
+            if(Integer.MAX_VALUE / 10 < res || Integer.MAX_VALUE / 10 == res && Integer.MAX_VALUE % 10 < digit)
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            
+            res = res*10 + digit;
+            i++; 
+        }
+        return res*sign;
     }
-    for(i=0;i<freq.length;i++){
-      if(str[i]!=' ' && str[i]!='0'){
-        System.out.println(str[i]+" "+freq[i]);
-      }
-    }
-  }
 }
