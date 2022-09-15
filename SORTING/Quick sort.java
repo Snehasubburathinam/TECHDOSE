b697935a8159d69615c6f61d6fb3addfc2d393ff
sneha <88367056+sneha512@users.noreply.github.com>
2022-09-15 12:42:12 +0530

    Create Quick sort.java

---
+++ SORTING/Quick sort.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Main {
+  public static int partition(int arr[],int start,int end){
+    int pivot=arr[end];
+    int i=start-1;
+    for(int j=start;j<end;j++){
+      if(arr[j]<pivot){
+          i++;
+          int t=arr[i];
+          arr[i]=arr[j];
+          arr[j]=t;
+      }
+    }
+      int t=arr[i+1];
+      arr[i+1]=arr[end];
+      arr[end]=t;
+      return (i+1); 
+  }
+  public static void quick(int arr[],int start,int end){
+    if(start<end){
+     int p= partition(arr,start,end);
+      quick(arr,start,p-1);
+      quick(arr,p+1,end);
+    }
+  }
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=sc.nextInt();
+    }
+    quick(arr,0,n-1);
+    for(int i=0;i<n;i++){
+      System.out.print(arr[i]+" ");
+    }
+  }
+}
+//Time Complexity
+//BC:O(NlogN)
+//WC:O(N^2)
