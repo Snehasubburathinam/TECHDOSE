05201f2bbddf34eb052b35e6535463b00a637207
sneha <88367056+sneha512@users.noreply.github.com>
2022-09-16 18:02:11 +0530

    Update Insertion sort.java

--- SORTING/Insertion sort.java
+++ SORTING/Insertion sort.java
@@ -7,21 +7,26 @@ class Main {
     for(int i=0;i<n;i++){
       arr[i]=sc.nextInt();
     }
-    List<Integer> lst=new ArrayList<>();
-    for(int i=0;i<n;i++){
-      lst.add(arr[i]);
-    for(int j=lst.size()-1;j>0;j--){
-        if(lst.get(j) >= lst.get(j-1)){
-           break;
-        }
-          Collections.swap(lst,j,j-1);
+    if(arr[0]>arr[1]){
+      int temp=arr[0];
+          arr[0]=arr[1];
+          arr[1]=temp;
+    }
+    for(int i=2;i<n;i++){
+      int j=i;
+        while((j>=1) && (arr[j]<arr[j-1])){
+          int temp=arr[j];
+          arr[j]=arr[j-1];
+          arr[j-1]=temp;
+          j--;
        }
     }
     for(int i=0;i<n;i++){
-      System.out.print(lst.get(i)+" ");
+      System.out.print(arr[i]+" ");
+    }
     }
   }
-}
+
 //Time Complexity:
 //BC:O(N)
 //WC:O(N^2)
