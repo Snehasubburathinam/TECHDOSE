51f0d57af4629fcfe00df2966db1cc3dba6f4b7b
sneha <88367056+sneha512@users.noreply.github.com>
2022-09-23 22:31:18 +0530

    Create 164.Maximum Gap.java

---
+++ SORTING/ASSIGNMENT/164.Maximum Gap.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+            if(nums.length==0)
+                return 0;
+            Arrays.sort(nums);
+            int res=0;
+            for(int i=1;i<nums.length;i++){
+            res=Math.max(res,nums[i]-nums[i-1]);
+            }
+            return res;
+    }
+}
