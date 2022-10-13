d4f7d54bbcb5d583a6531f5e5689a3205ba87d1e
sneha <88367056+sneha512@users.noreply.github.com>
2022-10-13 19:20:42 +0530

    Create WEEKLY CONTEST-239.java

---
+++ LEETCODE CONTESTS/WEEKLY CONTEST-239.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int n=nums.length;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                if(Math.abs(i-start)<n){
+                     n=Math.abs(i-start);
+                } 
+            }
+        }
+        return n;
+    }
+}
