305eb3de63c05cec091b9b015d62ec23602b8ec7
sneha <88367056+sneha512@users.noreply.github.com>
2022-10-14 12:04:44 +0530

    Create WEEKLY CONTEST-200.java

---
+++ LEETCODE CONTESTS/WEEKLY CONTEST-200.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int n=arr.length;
+        int ct=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n-1;j++){
+                if(Math.abs(arr[i]-arr[j])>a) continue;
+                for(int k=j+1;k<n;k++){
+                   if((Math.abs(arr[j]-arr[k])<=b) && (Math.abs(arr[i]-arr[k])<=c)){
+                       ct++;
+                   }
+                }
+            }
+        }
+        return ct;
+    }
+}
