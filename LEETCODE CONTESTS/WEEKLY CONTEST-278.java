e9ff8463772b5560a51b3abc709a3a158e8adede
sneha <88367056+sneha512@users.noreply.github.com>
2022-10-17 20:44:07 +0530

    Create WEEKLY CONTEST-278.java

---
+++ LEETCODE CONTESTS/WEEKLY CONTEST-278.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        for(int i:nums){
+            if(i==original){
+                original*=2;
+            }
+        }
+        return original;
+    }
+}
