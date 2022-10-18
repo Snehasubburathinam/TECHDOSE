6bddd934422b744df6921751fb9f04a3ca44ad0f
sneha <88367056+sneha512@users.noreply.github.com>
2022-10-18 19:29:46 +0530

    Update WEEKLY CONTEST-229.java

--- LEETCODE CONTESTS/WEEKLY CONTEST-229.java
+++ LEETCODE CONTESTS/WEEKLY CONTEST-229.java
@@ -2,7 +2,7 @@ class Solution {
     public String mergeAlternately(String word1, String word2) {
         String s="";
         int c=0,c1=0;
-        for(int i=0;i<word1.length+word2.length;i++){
+        for(int i=0;i<word1.length()+word2.length();i++){
         if(i<word1.length()){
             s+=word1.charAt(c);
             c++;
