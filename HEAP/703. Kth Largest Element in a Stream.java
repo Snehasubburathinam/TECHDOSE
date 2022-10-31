4d9523e123b856a8c286f94dd777b686aa640b3c
sneha <88367056+sneha512@users.noreply.github.com>
2022-10-31 19:14:38 +0530

    Create 703. Kth Largest Element in a Stream.java

---
+++ HEAP/703. Kth Largest Element in a Stream.java
@@ -0,0 +1,23 @@
+class KthLargest {
+    private static int k;
+    private PriorityQueue<Integer>heap;
+    
+    public KthLargest(int k, int[] nums) {
+     this.k=k;
+     heap=new PriorityQueue<>();
+        for(int n:nums){
+            heap.offer(n);
+        }
+        while(heap.size()>k){
+            heap.poll();
+        }
+    }
+    
+    public int add(int val) {
+        heap.offer(val);
+        if(heap.size()>k){
+            heap.poll();
+        }
+        return heap.peek();
+    }
+}
