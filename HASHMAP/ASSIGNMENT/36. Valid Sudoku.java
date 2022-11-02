868b3311f91c97a57330584bb9b4840612253667
sneha <88367056+sneha512@users.noreply.github.com>
2022-11-02 12:36:57 +0530

    Update 36. Valid Sudoku.java

--- HASHMAP/ASSIGNMENT/36. Valid Sudoku.java
+++ HASHMAP/ASSIGNMENT/36. Valid Sudoku.java
@@ -15,3 +15,31 @@ class Solution {
         return true;
     }
 }
+//
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+      Map<Integer,Integer>[]r=new HashMap[9];
+      Map<Integer,Integer>[]c=new HashMap[9];
+      Map<Integer,Integer>[]b=new HashMap[9];
+        for(int i=0;i<9;i++){
+            r[i]=new HashMap<Integer,Integer>();
+            c[i]=new HashMap<Integer,Integer>();
+            b[i]=new HashMap<Integer,Integer>();
+        }
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]!='.'){
+                    int n=board[i][j];
+                    int bx=(i/3)*3+(j/3);
+                    r[i].put(n,r[i].getOrDefault(n,0)+1);
+                    c[j].put(n,c[j].getOrDefault(n,0)+1);
+                    b[bx].put(n,b[bx].getOrDefault(n,0)+1);
+                    if(r[i].get(n)>1 || c[j].get(n)>1 || b[bx].get(n)>1){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
