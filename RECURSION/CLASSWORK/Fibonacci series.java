03543eacc720de3df7cd379d24f9baee03f1981c
sneha <88367056+sneha512@users.noreply.github.com>
2022-11-04 18:50:21 +0530

    Create Fibonacci series.java

---
+++ RECURSION/CLASSWORK/Fibonacci series.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    System.out.println(Fibonacci(n));
+  }
+  //memoization
+  public static int Fibonacci(int n){
+    int []fib=new int[n+1];
+    fib[0]=0;
+    fib[1]=1;
+    for(int i=2;i<=n;i++){
+      fib[i]=fib[i-1]+fib[n-2];
+    }
+    return fib[n];
+  }
+  //recursion
+  public static int Fibonacci(int n){
+    if(n==0||n==1){
+      return n;
+    }
+    return Fibonacci(n-1)+Fibonacci(n-2);
+  }
+}
